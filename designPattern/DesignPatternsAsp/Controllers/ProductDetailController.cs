57043bc8695758ec60f7d99677e708ba65d5fc68
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2022-10-26 16:48:51 -0300

    02_FactoryPattern - Se agrega ejemplo MVC

---
+++ designPattern/DesignPatternsAsp/Controllers/ProductDetailController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Tools.Earnd;
+
+namespace DesignPatternsAsp.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+
+            //factories
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.30m, 20);
+
+
+            //products
+            var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+
+            //total
+            ViewBag.totalLocal = total + localEarn.Earn(total);
+            ViewBag.totalForeign = total + foreignEarn.Earn(total);
+
+            return View();
+        }
+    }
+}
---
+++ designPattern/Tools/Earnd/IEarn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earnd
+{
+    public interface IEarn
+    {
+        public decimal Earn(decimal amount);
+
+    }
+}
